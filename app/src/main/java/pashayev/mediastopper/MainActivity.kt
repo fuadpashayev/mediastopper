15698613ecb111cd7ca750e0dde4af8253283cfb
unknown <fuad.pashayev12@gmail.com>
2018-10-06 14:25:58 +0400

    en son

--- app/src/main/java/pashayev/mediastopper/MainActivity.kt
+++ app/src/main/java/pashayev/mediastopper/MainActivity.kt
@@ -1,5 +1,8 @@
 package pashayev.mediastopper
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.media.AudioManager
@@ -12,6 +15,10 @@ import android.os.Handler
 import android.os.IBinder
 import android.os.Parcelable
 import android.util.Log
+import android.os.CountDownTimer
+import android.app.NotificationChannel
+import android.os.Build
+import android.support.v4.app.NotificationCompat
 
 
 class mediaService : Service() {
@@ -22,28 +29,78 @@ class mediaService : Service() {
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        Log.d("----------a",intent.extras.getParcelable<Parcelable>("second").toString())
-//        Handler().postDelayed({
-//            val am = getSystemService(Context.AUDIO_SERVICE) as AudioManager
-//            am.requestAudioFocus(focusChangeListener,
-//                    AudioManager.STREAM_MUSIC,
-//                    AudioManager.AUDIOFOCUS_GAIN)
-//        },second)
+        val timeVal = intent.getLongExtra("timeVal",100)*1000
+        Handler().postDelayed({
+            val am = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+            am.requestAudioFocus(focusChangeListener,
+                    AudioManager.STREAM_MUSIC,
+                    AudioManager.AUDIOFOCUS_GAIN)
+        },timeVal)
+
         return Service.START_STICKY
     }
 }
 class MainActivity : AppCompatActivity() {
-
+    var active=false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         save.setOnClickListener {
-            val second = second.text
-            val intent = Intent(this,mediaService::class.java)
-            intent.putExtra("second",second)
-            startService(intent)
+            if(!active) {
+                val time = second.text.toString().toLong()
+                val type = spinner.selectedItemPosition
+                val timeVal = when (type) {
+                    0 -> time * 60
+                    1 -> time * 3600
+                    else -> time * 60
+                }
+                object : CountDownTimer(timeVal * 1000, 1000) {
 
+                    override fun onTick(millisUntilFinished: Long) {
+                        remainTime.text = "Qalan vaxt: ${millisUntilFinished / 1000}"
+
+
+                        val mBuilder = NotificationCompat.Builder(this@MainActivity, "notify_ms")
+                        val ii = Intent(this@MainActivity, MainActivity::class.java)
+                        val pendingIntent = PendingIntent.getActivity(this@MainActivity, 0, ii, 0)
+                        val bigText = NotificationCompat.BigTextStyle()
+                        bigText.bigText("${millisUntilFinished / 1000} saniyə")
+                        bigText.setBigContentTitle("Qalan vaxt")
+                        bigText.setSummaryText("MediaStopper vaxt məlumatı")
+                        mBuilder.setContentIntent(pendingIntent)
+                        mBuilder.setSmallIcon(R.mipmap.ic_launcher_round)
+                        mBuilder.setPriority(Notification.PRIORITY_MAX)
+                        mBuilder.setStyle(bigText)
+
+                        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                        {
+                            val channel = NotificationChannel("notify_ms", "Mediastopper",
+                                    NotificationManager.IMPORTANCE_DEFAULT)
+                            mNotificationManager.createNotificationChannel(channel)
+                        }
+                        mNotificationManager.notify(0, mBuilder.build())
+
+                    }
+
+                    override fun onFinish() {
+                        remainTime.setText("Bitdi!")
+                        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                        mNotificationManager.cancel(0)
+                        active=false
+                    }
+
+                }.start()
+
+
+
+                val intent = Intent(this, mediaService::class.java)
+                intent.putExtra("timeVal", timeVal)
+                startService(intent)
+
+                active=true
+            }
         }
 
     }
